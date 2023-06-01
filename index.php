a42c282aae457188c5bca17ec5d2d2724d5e8cf1
Vishal10031 <99544171+Vishal10031@users.noreply.github.com>
2023-06-01 14:23:07 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,275 +1,133 @@
-<?php require("libs/fetch_data.php");?>
 <!DOCTYPE html>
-<html lang="zxx">
+<html>
 <head>
-	<title><?php getwebname("titles"); echo"|"; gettagline("titles");?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">
-	<link id="browser_favicon" rel="shortcut icon" href="blogadmin/images/<?php geticon("titles"); ?>">
-	<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
-	<meta name="keywords" content="<?php getkeywords("titles");?>" />
-	<script>
-		addEventListener("load", function () {
-			setTimeout(hideURLbar, 0);
-		}, false);
-
-		function hideURLbar() {
-			window.scrollTo(0, 1);
-		}
-	</script>
-	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-	<link rel="stylesheet" href="css/jquery.desoslide.css">
-	<link href="css/style.css" rel='stylesheet' type='text/css' />
-	<link href="css/fontawesome-all.css" rel="stylesheet">
-	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
-	rel="stylesheet">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+	<title>BLOG ADMIN STATISTICS</title>
+	<link id="browser_favicon" rel="shortcut icon" href="">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap/js/jquery.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </head>
-
 <body>
-	<?php include("header.php");?>
-	<?php include("banner.php");?>
-	<section class="bottom-slider">
-		<div class="course_demo1">
-			<ul id="flexiselDemo1">
-				<?php getbottomsliderposts("blogs");?>
-			</ul>
-		</div>
-		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-						<div class="embed-responsive embed-responsive-21by9">
-							<iframe src="https://www.youtube.com"></iframe>
-						</div>
-					</div>
+	<div class="container">
+		<div class="row">
+			<!--unique visitors count-->
+			<div class="alert alert-success">
+				<strong><?php 
+				require_once('conn.php');
+				$sql = "SELECT COUNT(DISTINCT ip_address) AS alias FROM visitor_info";
+				$query = $db->prepare($sql);
+				$query->execute();
+				$unique_visitors = $query->fetch()['alias'];
+				echo "TOTAL NUMBER OF UNIQUE VISITORS:".$unique_visitors;
 
+				?></strong>
+			</div>
+			<!--get most viewed page-->
+			<div class="alert alert-info">
+				<strong><?php 
+				require_once('conn.php');
+				$sql = "SELECT MAX(count) AS maximum FROM page_hits";
+				$query = $db->prepare($sql);
+				$query->execute();
+				$maxcount = $query->fetch()['maximum'];
+				echo "HIGHEST PAGE VIEWS :".$maxcount;
+
+				?></strong>
+			</div><br>
+			<div class="panel panel-primary">
+				<div class="panel-heading text-center">LATEST PAGE VIEWS&nbsp;&nbsp;(TOTAL PAGES VIEWED:<?php require_once("count_pages.php"); ?>)</div>
+				<div class="panel-body">
+					<div class="table-responsive">
+						<table class="table">
+							<tr class="info">
+								<th>Page Name</th>
+								<th>Total Views</th>
+							</tr>
+							<?php 
+							include("connect.php");
+							try {
+//connect to mysql
+								$con=new PDO($dsn,$username,$password);
+								$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+								
+							} catch (Exception $ex) {
+								echo 'Not Connected '.$ex->getMessage();
+							}
+
+//mysql select query
+							$stmt=$con->prepare('SELECT * FROM page_hits LIMIT 30');
+							$stmt->execute();
+							$resources=$stmt->fetchAll();
+							$row_count=$stmt->rowCount();
+							if ($row_count==0) {
+		# code...
+								$jibu="No content Yet";
+							}
+							foreach ($resources as $resource) {
+								echo '
+								<tr>
+								<td>'.$resource['page'].'</td>
+								<td>'.$resource['count'].'</td>
+								</tr>';
+
+
+							}
+							?>
+						</table>
+					</div> 
 				</div>
 			</div>
-		</div>
-	</section>
-	<!--/main-->
-	<section class="main-content-w3layouts-agileits">
-		<div class="container">
-			<div class="row">
-				<!--left-->
-				<div class="col-lg-8 left-blog-info-w3layouts-agileits text-left">
-					<!--grid blogs below-->
-					<div class="blog-girds-sec">
-						<div class="row">
-							<?php getblogridposts("blogs");?>
-							<!--bloggrids-->
-						</div>
-					</div>
+		</div><!--row-->
+		<div class="row">
+			<div class="panel panel-success">
+				<div class="panel-heading text-center">LATEST VISITOR DETAILS&nbsp;&nbsp;(TOTAL RECORDS:<?php require_once("count_user_stats.php"); ?>)</div>
+				<div class="panel-body">
+					<div class="table-responsive">
+						<table class="table">
+							<tr class="info">
+								<th>IP ADDRESS</th>
+								<th>USER AGENT</th>
+								<th>TIME ACCESSED</th>
+							</tr>
+							<?php 
+							include("connect.php");
+							try {
+//connect to mysql
+								$con=new PDO($dsn,$username,$password);
+								$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+								
+							} catch (Exception $ex) {
+								echo 'Not Connected '.$ex->getMessage();
+							}
+
+//mysql select query
+							$stmt=$con->prepare('SELECT * FROM visitor_info ORDER BY time_accessed DESC LIMIT 10');
+							$stmt->execute();
+							$resources=$stmt->fetchAll();
+							$row_count=$stmt->rowCount();
+							if ($row_count==0) {
+		# code...
+								$jibu="No content Yet";
+							}
+							foreach ($resources as $resource) {
+								echo '
+								<tr>
+								<td>'.$resource['ip_address'].'</td>
+								<td>'.$resource['user_agent'].'</td>
+								<td>'.$resource['time_accessed'].'</td>
+								</tr>';
+
+
+							}
+							?>
+						</table>
+					</div> 
 				</div>
-				<!--//left-->
-				<!--right-->
-				<aside class="col-lg-4 agileits-w3ls-right-blog-con text-right">
-					<div class="right-blog-info text-left">
-						<h4><strong>Categories</strong></h4>
-						<ul class="list-group single">
-							<?php countcategories();?>
-						</ul>
-						<div class="tech-btm widget_social">
-							<h4>Stay Connected</h4>
-							<ul>
-
-								<li>
-									<a class="twitter" href="<?php getlinks("links","twitter");?>">
-										<i class="fab fa-twitter"></i>
-										<span class="count"></span> Twitter</a>
-									</li>
-									<li>
-										<a class="facebook" href="<?php getlinks("links","facebook");?>">
-											<i class="fab fa-facebook-f"></i>
-											<span class="count"></span> Facebook</a>
-										</li>
-										<li>
-											<a class="dribble" href="<?php getlinks("links","dribble");?>">
-												<i class="fab fa-dribbble"></i>
-
-												<span class="count"></span> Dribble</a>
-											</li>
-											<li>
-												<a class="pin" href="<?php getlinks("links","pinterest");?>">
-													<i class="fab fa-pinterest"></i>
-													<span class="count"></span> Pinterest</a>
-												</li>
-
-											</ul>
-										</div>
-										<div class="tech-btm">
-											<h4>Older Posts</h4>
-											<?php getolderposts("blogs");?>
-											<!--olderpostsendhere-->
-										</div>
-									</div>
-								</aside>
-								<!--//right-->
-							</div>
-						</div>
-					</section>
-					<!--//main-->
-					<!--/middle-->
-					<section class="middle-sec-agileinfo-w3ls">
-						<div class="container">
-							<div class="row inner-sec">
-								<div class="col-md-4 news-left">
-									<ul id="demo1_thumbs" class="list-inline">
-										<!--get latest four-->
-										<?php getfour("blogs");?>
-									</ul>
-								</div>
-								<div id="demo1_main_image" class="col-md-8  news-right"></div>
-								<div class="clearfix"> </div>
-							</div>
-						</div>
-
-					</section>
-					<!--//middle-->
-					<!---->
-					<section class="main-content-w3layouts-agileits">
-						<div class="container">
-							<div class="row">
-								<!--left-->
-								<div class="col-lg-8 left-blog-info-w3layouts-agileits text-left">
-									<?php getonelatest("blogs");?>
-								</div>
-								<!--//left-->
-								<!--right-->
-								<aside class="col-lg-4 agileits-w3ls-right-blog-con text-left">
-									<div class="tech-btm">
-										<h4>Editor's Choice</h4>
-										
-										<?php geteditorschoice("editors_choice"); ?>
-										
-									</div>
-									
-								</aside>
-								<!--//right-->
-							</div>
-						</div>
-					</section>
-					<!--//main-->
-					<?php include("footer.php");?>
-					<!---->
-					<!-- js -->
-					<script src="js/jquery-2.2.3.min.js"></script>
-					<!-- //js -->
-					<!-- desoslide-JavaScript -->
-					<script src="js/jquery.desoslide.js"></script>
-					<script>
-						$('#demo1_thumbs').desoSlide({
-							main: {
-								container: '#demo1_main_image',
-								cssClass: 'img-responsive'
-							},
-							effect: 'sideFade',
-							caption: true
-						});
-					</script>
-
-					<!-- requried-jsfiles-for owl -->
-					<script>
-						$(window).load(function () {
-							$("#flexiselDemo1").flexisel({
-								visibleItems: 3,
-								animationSpeed: 1000,
-								autoPlay: true,
-								autoPlaySpeed: 3000,
-								pauseOnHover: true,
-								enableResponsiveBreakpoints: true,
-								responsiveBreakpoints: {
-									portrait: {
-										changePoint: 480,
-										visibleItems: 1
-									},
-									landscape: {
-										changePoint: 640,
-										visibleItems: 2
-									},
-									tablet: {
-										changePoint: 768,
-										visibleItems: 3
-									}
-								}
-							});
-
-						});
-					</script>
-					<script>
-						$(window).load(function () {
-							$("#flexiselDemo2").flexisel({
-								visibleItems: 3,
-								animationSpeed: 1000,
-								autoPlay: true,
-								autoPlaySpeed: 3000,
-								pauseOnHover: true,
-								enableResponsiveBreakpoints: true,
-								responsiveBreakpoints: {
-									portrait: {
-										changePoint: 480,
-										visibleItems: 1
-									},
-									landscape: {
-										changePoint: 640,
-										visibleItems: 2
-									},
-									tablet: {
-										changePoint: 768,
-										visibleItems: 3
-									}
-								}
-							});
-
-						});
-					</script>
-					<script src="js/jquery.flexisel.js"></script>
-					<!-- //password-script -->
-					<!--/ start-smoth-scrolling -->
-					<script src="js/move-top.js"></script>
-					<script src="js/easing.js"></script>
-					<script>
-						jQuery(document).ready(function ($) {
-							$(".scroll").click(function (event) {
-								event.preventDefault();
-								$('html,body').animate({
-									scrollTop: $(this.hash).offset().top
-								}, 900);
-							});
-						});
-					</script>
-					<!--// end-smoth-scrolling -->
-
-					<script>
-						$(document).ready(function () {
-							
-							var defaults = {
-							  			containerID: 'toTop', // fading element id
-										containerHoverID: 'toTopHover', // fading element hover id
-										scrollSpeed: 1200,
-										easingType: 'linear' 
-									};
-									
-
-									$().UItoTop({
-										easingType: 'easeOutQuart'
-									});
-
-								});
-							</script>
-							<a href="#home" class="scroll" id="toTop" style="display: block;">
-								<span id="toTopHover" style="opacity: 1;"> </span>
-							</a>
-
-							<!-- //Custom-JavaScript-File-Links -->
-							<script src="js/bootstrap.js"></script>
-
-
-						</body>
-
-						</html>
+			</div>
+		</div><!--row-->
+	</div>
+</body>
+</html>
